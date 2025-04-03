bb171bf00b0c0e6bdbf53fb2e7cd42b0a556ae79
unknown <skes01149@naver.com>
2025-04-03 17:42:47 +0900

    실행 설정 파일 추가

---
+++ src/main/java/blog/blogstudy/Blog.java
@@ -0,0 +1,12 @@
+package blog.blogstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog {
+    public static void main(String[] args) {
+        SpringApplication.run(Blog.class, args);
+    }
+}
+
