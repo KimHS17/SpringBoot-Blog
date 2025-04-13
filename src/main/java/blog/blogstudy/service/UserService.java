78520046b36c435c090520a79305b926c10ab974
KimHS17 <skes01149@naver.com>
2025-04-13 22:34:00 +0900

    회원가입, 로그인, 로그아웃 구현

---
+++ src/main/java/blog/blogstudy/service/UserService.java
@@ -0,0 +1,24 @@
+package blog.blogstudy.service;
+
+import blog.blogstudy.domain.User;
+import blog.blogstudy.dto.AddUserRequest;
+import blog.blogstudy.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
